5e6c4b66cf84b6dff46b9f509418c377b2399175
prantobiswas321 <rahulbd321@gmail.com>
2021-08-19 11:37:27 +0600

    typed numbers displayed

--- js/pin.js
+++ js/pin.js
@@ -13,3 +13,19 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const inputNumber = document.getElementById('typed-numbers');
+    const number = event.target.innerText;
+    if (isNaN(number)) {
+        if (number == 'C') {
+            inputNumber.value = '';
+        }
+    }
+    else {
+        // const inputNumber = document.getElementById('typed-numbers');
+        const prevNum = inputNumber.value;
+        const newNum = prevNum + number;
+        inputNumber.value = newNum;
+    }
+});
